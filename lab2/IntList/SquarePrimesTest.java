60b98bddf36624f559ee5ad5d5bd3528bbf1908f
WeiLok <weilok629@gmail.com>
2024-05-03 13:09:00 +0800

    SquarePrimes debugged

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,18 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testMoreSquarePrimes() {
+        IntList lst1 = IntList.of(14, 15, 16, 10, 18);
+        boolean changed1 = IntListExercises.squarePrimes(lst1);
+        assertEquals("14 -> 15 -> 16 -> 10 -> 18", lst1.toString());
+        assertFalse(changed1);
+
+        IntList lst2 = IntList.of(1, 2, 3, 4, 5);
+        boolean changed2 = IntListExercises.squarePrimes(lst2);
+        assertEquals("1 -> 4 -> 9 -> 4 -> 25", lst2.toString());
+        assertTrue(changed2);
+
+    }
 }
